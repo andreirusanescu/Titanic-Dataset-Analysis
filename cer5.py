979a4b275cea7e89933c8ba34477fd51f4c56ef7
Andrei Rusanescu <andreirusanescu154@gmail.com>
2024-05-24 16:14:27 +0300

    Cerinta 5

---
+++ cer5.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('train.csv')
+bins = [0, 20, 40, 60, data['Age'].max()]
+index = [0, 1, 2, 3]
+
+data['age_index'] = pd.cut(data['Age'], bins = bins, labels=index)
+age_count = data['age_index'].value_counts().sort_index()
+
+labels = ['0-20', '21-40', '41-60', '61+']
+plt.figure(figsize=(8, 6))
+plt.bar(labels, age_count, color='skyblue', edgecolor='black')
+plt.xlabel('Categorii de varsta')
+plt.ylabel('Numarul total de pasageri')
+plt.title('Numarul de pasageri / categorie')
+plt.show()
