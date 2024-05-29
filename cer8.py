338ed2830a88529eb7e5b04717430f91cd557544
Andrei Rusanescu <andreirusanescu154@gmail.com>
2024-05-29 22:34:43 +0300

    Modificat outputul

--- cer8.py
+++ cer8.py
@@ -4,13 +4,11 @@ data = pd.read_csv('train.csv')
 missing_values = data.isnull().sum()
 filtered = missing_values[missing_values != 0]
 
-print(data.isna().sum())
-print(data)
 for column in filtered.index:
     if data[column].dtype == 'float64' or data[column].dtype == 'int64':
         data[column] = data[column].fillna(data[column].mean())
     else:
         data[column] = data[column].fillna(data[column].mode()[0])
 
-print(data.isna().sum())
-print(data)
+# exclud indexul din afisare
+data.to_csv('cer8.csv', index=False)
